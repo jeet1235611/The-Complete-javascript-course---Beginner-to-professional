6524a47cabc5bb377f0bcdd120695f02074b2684
acoustic_jeet <js.jitendra11@gmail.com>
2018-05-22 18:30:19 +0530

    Create largerOfTwoNumbers.js

---
+++ largerOfTwoNumbers.js
@@ -0,0 +1,9 @@
+//Concepts used
+//conversion of string to number
+//use of ternary.
+
+var a = prompt("First");
+var b = prompt("Second");
+var message = (Number(a) > Number(b)) ? "First is Larger" + a : "First is smaller " + b;
+alert(message);
+
