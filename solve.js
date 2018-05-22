c8e5c7f75efd40f6cf60c5302f70eba64b5cbbc6
acoustic_jeet <js.jitendra11@gmail.com>
2018-05-22 19:01:21 +0530

    Create solve.js

---
+++ solve.js
@@ -0,0 +1,15 @@
+Que!)
+Find which of the following statements are true:
+
+
+var a = 5;
+var b = 10;
+var c = "5";
+
+b++;
+var d = ((b%5) > 0) ? "odd" : "even";
+
+console.log(typeof a == "number");
+console.log(typeof c == typeof "number");
+console.log((d=="odd" && b>10 && b >=(a*2)));
+
