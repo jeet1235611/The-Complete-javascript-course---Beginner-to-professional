2a80bee4fe808d603384d7dcaa34962f32da082c
acoustic_jeet <js.jitendra11@gmail.com>
2018-05-23 09:37:05 +0530

    Create arrays.js

---
+++ arrays.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<script>
+		
+		var myArray = ["string", 100, false];
+		var myArray3 = [];
+
+		var myArray1 = new Array("string", 100, false);
+		var myArray2 = new Array();
+
+		console.log(myArray[0]);
+		myArray[1] = "New Value";
+		console.log(myArray);
+		
+
+	</script>
+
+</body>
