3d4b0650f16beeb960d55e05ae1dc6587168c3a6
acoustic_jeet <js.jitendra11@gmail.com>
2018-05-23 10:46:44 +0530

    Create dynamicTask.js

---
+++ dynamicTask.js
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<script>
+		
+		var task = ["studying", "coding", "javascript_Learning", "eating", "sleeping", "competitive coding", "walking", "yoga", "exercise", "bathing"];
+
+		var progress = ["done", "in progress", "to do"];
+
+		var question = prompt("Which task 1 - 7?");
+		var question2 = prompt("status1 = done 2 = in progress 3 = to do?");
+
+		console.log("today you will " + task[question1-1] + " status of task = " + progress[question2-1]);
+
+
+	</script>
+
+</body>
+</html>
